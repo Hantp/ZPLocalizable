69248d806209fcce6b7053578fc61ef3a406454a
Zepiao Han <zepiah@ZepiaodeMacBook-Pro.local>
2022-02-12 13:39:47 -0800

    Add thisConstants

--- Sources/LocalizablePackage/LocalizablePackage.swift
+++ Sources/LocalizablePackage/LocalizablePackage.swift
@@ -3,8 +3,10 @@ import Foundation
 public struct LocalizablePackage {
     public private(set) var text = "Hello, World!"
     
-    public let greeting = NSLocalizedString("Greet", tableName: nil, bundle: Bundle.main , value: "nihao", comment: "greeting")
-
     public init() {
     }
+}
+
+public class ThisConstants {
+    public static let greeting = NSLocalizedString("Greet", tableName: nil, bundle: Bundle.main , value: "nihao", comment: "greeting")
 }

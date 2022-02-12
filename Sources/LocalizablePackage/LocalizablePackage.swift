763e84ff1ef38c047d9a24528949acfec4c11f8a
Zepiao Han <zepiah@ZepiaodeMacBook-Pro.local>
2022-02-12 13:36:47 -0800

    add foundation

--- Sources/LocalizablePackage/LocalizablePackage.swift
+++ Sources/LocalizablePackage/LocalizablePackage.swift
@@ -1,10 +1,10 @@
+import Foundation
+
 public struct LocalizablePackage {
     public private(set) var text = "Hello, World!"
+    
+    public let greeting = NSLocalizedString("Greet", tableName: nil, bundle: Bundle.main , value: "nihao", comment: "greeting")
 
     public init() {
     }
-}
-
-public class YourPackageBundle {
-   public static let yourPackageBundle = Bundle.module
 }
